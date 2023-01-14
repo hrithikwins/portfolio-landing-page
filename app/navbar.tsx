048f3dd334c4dcd730941ed41b4285242a851f04
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-14 20:39:10 +0530

    header layout

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -14,9 +14,9 @@ const Navbar = ({}) => {
     <>
       <div className="bg-white flex flex-row justify-between items-center px-28 py-10 w-full  h-90 ">
         <div className="font-bold text-[38px]">Samar</div>
-        <div className="bg-white hidden lg:flex flex-row space-x-8 font-bold text-xl leading-[30px] ">
+        <div className="bg-white  lg:flex flex-row space-x-8 font-bold text-xl leading-[30px] ">
           {navbarItems.map((item) => (
-            <div>{item.name}</div>
+            <div className="hidden">{item.name}</div>
           ))}
           <div className="">
             <a href="https://linkedin.com/in/hrithikwins" target="_blank" className="font-bold text-[38px] lg:text-[22px] bg-primary px-6 py-3 text-white border border-primary rounded-lg">
--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,27 @@ const Page = () => {
     <div>
       <div className=" bg-gradient-to-b from-[#FFF2C5] h-screen">
         {/* navbar */}
-        <Navbar/>
+        <Navbar />
         {/* end navbar */}
         <div className="container">
-          <div className="flex"></div>
+          <div className="flex flex-col lg:flex-row">
+            {/* tagline and description */}
+            <div className="flex flex-col lg:w-1/2">
+              <h1>Hello this is Samar</h1>
+              <div className="flex flex-row">
+                {/* button 1 */}
+                <div className="bg-primary px-6 py-3 text-white border border-primary rounded-lg">Say Hello</div>
+                <div className="bg-primary px-6 py-3 text-white border border-primary rounded-lg">Connct</div>
+              </div>
+            </div>
+            {/* image  */}
+            <div className="flex flex-row">
+              <Image src="/samar.png" width={500} height={500} alt={""}  />
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 export default Page;
-
-
-
